2e014867807f409cee394b00582ffbb4e33dee55
Jingyuan Li <jingyuanli94@gmail.com>
2018-02-17 11:18:44 -0500

    Code clarified

--- GreedySearch.py
+++ GreedySearch.py
@@ -1,5 +1,6 @@
 import csv
 import math
+import numpy as np
 from random import randint
 
 def readCSV(filename):
@@ -10,23 +11,25 @@ def readCSV(filename):
         groupList = groupList[2:len(groupList)]
     return groupList, listHead
 
-# calculate euclidean distance of two lists
 def euclidean(list0, list1, weight):
+    '''
+    Calculate euclidean distance of two lists (sum of the distances between each attribute)
+    @parameter: list attributes
+    '''
     distance = 0
-    # totalWeight = 0
-    # for i in range(len(weight)):
-    #     totalWeight += weight[i]
     for i in range(len(list0)):
         # distance += math.fabs((float(list0[i]) - float(list1[i]))) * float(weight[i])
         distance += math.pow((float(list0[i]) - float(list1[i])), 2) * float(weight[i])
-    return math.sqrt(distance)
+    return math.sqrt(abs(distance))
 
-# calculate similar pair in a group of list (each group has three cases)
 def dist(list0, list1, list2, weight):
-    length = len(list0)
-    list0 = list0[1:length]
-    list1 = list1[1:length]
-    list2 = list2[1:length]
+    '''
+    Find out the similar patient pair in a group (each group has three cases)    
+    @parameter: 
+    '''
+    list0 = list0[1:]
+    list1 = list1[1:]
+    list2 = list2[1:]
     dist = []
     dist.append(euclidean(list0, list1, weight))
     dist.append(euclidean(list1, list2, weight))
@@ -39,16 +42,23 @@ def dist(list0, list1, list2, weight):
         return "02"
 
 def calAccuracy(pairs1, pairs2):
+    '''
+    Calculate the accuracy between two pairs  
+    @parameter: 
+    '''
     if len(pairs1) != len(pairs2):
-        print "Pairs length not match!"
+        print ("Pairs length not match!")
     total = len(pairs1)
     count = 0
     for i in range(total):
         if pairs1[i] == pairs2[i]:
             count += 1
-    return float(count) / float(total)
+    return float(count) / total
 
 def calSimilarPairs(groupList, weight):
+    '''
+    Find out similar patient pairs in a group list
+    '''
     similarPairs = []
     for i in range(len(groupList)/3):
         list0 = groupList[i]
@@ -59,98 +69,101 @@ def calSimilarPairs(groupList, weight):
 
 if __name__ == "__main__":
     groupList, listHead = readCSV("CopyofRandGroup.csv")
-    # print groupList
     coloums = len(groupList[0])
     rows = len(groupList)
-    #
-    # Uniform each coloum
-    #
-    maxAttr = 0
-    for i in range(coloums):
-        if i > 0:
-            for j in range(rows):
-                if float(groupList[j][i]) > float(maxAttr):
-                    maxAttr = float(groupList[j][i])
-            for j in range(rows):
-                groupList[j][i] = float(float(groupList[j][i]) / maxAttr)
-            maxAttr = 0
-    print groupList
-    #
-    # weight = 1, calculate accuracy
-    #
-    weight = []
-    for i in range(len(groupList[0]) - 1):
-        weight.append(1)
-    similarPairs = calSimilarPairs(groupList, weight)
-    #
+
+    # Uniform each coloum attributes
+    for i in range(1, coloums):
+        maxAttr = max(groupList[:][i])
+        groupList[:][i] = [float(attr) / float(maxAttr) for attr in groupList[:][i]]
+    
     # Similar pairs of patients in each group labeled by doctor
-    #
     similarPairs_Dr = ['12','02','12','02','12','02','01','01','12','02','02','01','01','02','01','12','01','12','01','01','02','12','12','12','12','12','01','12','12','12','01','12','02','02','12','12','02','12','01','12','12']
+
+    best = 0                        # Maximum improvement (global)
+    learningRate = 1.0              # Learning rate
+    weight = []                     # attribute weights 
+
+    # Initialize weights
+    weight = list(np.zeros(len(groupList[0]) - 1))
+    n = len(weight)
+
+    similarPairs = calSimilarPairs(groupList, weight)
     accuracy = calAccuracy(similarPairs, similarPairs_Dr)
-    best = 0
+
+    print (weight)
+    print (accuracy)
+    print ("----------------------------")
+
     while True:
-        #
-        # Reset weight to [1, 1, ..., 1]
-        #
-        weight = []
-        print len(groupList)
-        for i in range(len(groupList[0]) - 1):
-            weight.append(1)
-        accuracy_change = []
-        n = 0
+        accuracy = 0
         count = 0
         improve = 0
+        record_acc = []
         acc = []
-        weightIncrease = []
         while True:
-            n += 1
-            #
             # Try to increase each weight to get the maxImprove weight
-            #
-            for i in range(len(weight)):
-                weight[i] += 1
+            for i in range(n):
+                weight[i] += learningRate
                 similarPairs_new = calSimilarPairs(groupList, weight)
-                accuracy_new = calAccuracy(similarPairs_new, similarPairs_Dr)
-                accuracy_change.append(accuracy_new - accuracy)
-                weight[i] -= 1
-            maxImprove = accuracy_change[accuracy_change.index(max(accuracy_change))]
-            print accuracy + maxImprove
-            acc.append(accuracy + maxImprove)
-            print weight
-            with open("weight.csv",'a') as File:
-                File.write(str(accuracy + maxImprove) + '\t' + str(weight)[1:-1] + '\n')
+                accuracy = calAccuracy(similarPairs_new, similarPairs_Dr)
+                acc.append(accuracy)
+                weight[i] -= learningRate
 
-            if maxImprove != improve:
-                improve = maxImprove
+            # Try to decrease
+            for i in range(n):
+                if weight[i] - learningRate > 0:
+                    weight[i] -= learningRate
+                    similarPairs_new = calSimilarPairs(groupList, weight)
+                    accuracy = calAccuracy(similarPairs_new, similarPairs_Dr)
+                    acc.append(accuracy)
+                    weight[i] += learningRate
+                else:
+                    acc.append(0)
+
+            maxAcc = max(acc)                               # Maximum accuracy
+            alpha = acc.count(maxAcc)                       # Count the number of the maximum accuracy
+
+            for i in range(2 * n):
+                if acc[i] == maxAcc and i < n:
+                    weight[i] += learningRate / alpha       # Weight increment
+                elif acc[i] == maxAcc and i >= n:
+                    weight[i - n] -= learningRate / alpha   # Weight subtraction
+            
+            print ("----------")
+
+            # calculate updated accuracy
+            similarPairs_new = calSimilarPairs(groupList, weight)
+            accuracy = calAccuracy(similarPairs_new, similarPairs_Dr)
+
+            # print updated accuracy
+            print (accuracy)
+            record_acc.append(maxAcc)
+            print (weight)
+
+            # record weights in every step
+            with open("weight.csv",'a') as File:
+                File.write(str(maxAcc) + ',' + str(weight)[0:-1] + '\n')
+
+            if maxAcc != improve:
+                improve = maxAcc
                 count = 0
             else:
                 count += 1
-            
-            #
-            # randomly increase weight
-            #
-            maxIndex = []
-            for j in range(len(weight)):
-                if accuracy_change[j] == maxImprove:
-                    maxIndex.append(j)
-            r = randint(0, (len(maxIndex)-1))
-            weight[maxIndex[r]] += 1
 
-            # print weight
+            acc = []
             
-            accuracy_change = []
-            n += 1
-            
+            # record accuracy improvements and break the loops
             if count >= 200:
-                if (accuracy + maxImprove) > best:
-                    best = accuracy + maxImprove
+                if (maxAcc) > best:
+                    best = maxAcc
                     print best
                     print weight
                     print "---------------------------------------------------------"
                     with open("weight.csv", "a") as File:
                         File.write('--------------------------------------------------' + '\n')
                     with open("output.csv",'wb') as resultFile:
-                        resultFile.write(str(acc)[1:-1] + '\n')
+                        resultFile.write(str(record_acc)[1:-1] + '\n')
                 break
         
     
